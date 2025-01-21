8e0aff41f1fc32073f1290076971ff513f9bc99a
Oscar Garcia <oskar_pumauni@hotmail.com>
2025-01-21 17:29:49 -0600

    Format changes

--- main.tf
+++ main.tf
@@ -1,38 +1,38 @@
 
 module "s3_bucket" {
-    source = "./modules/s3_bucket"
-    aws_region = "us-east-1"
-    environment = "dev"
-    bucket_name = "xideraloscar-bucket-tf-practice"
-    web_index_file_name = "index.html"
-    web_error_file_name = "error.html"
-    bucket_policy = jsonencode({
-        Version = "2012-10-17",
-        Id      = "HTTP referer policy example",
-        Statement = [
-        {
-            Sid       = "AllowOnlySpecificReferers",
-            Effect    = "Allow",
-            Principal = "*",
-            Action    = [
-                "s3:GetObject",
-                "s3:GetObjectVersion"
-            ],
-            Resource = "${aws_s3_bucket.main_bucket.arn}/*",
-        }]
-        })
-    web_html = {
-        path = ".\\web_files\\index.html", 
-        name = "index.html",
-    }
-    web_css = {
-        path = ".\\web_files\\style.css"
-        name = "style.css"
-    }
-    web_js = {
-        path = ".\\web_files\\function.js"
-        name = "function.js"
-    }
-    block_public_access = false
+  source              = "./modules/s3_bucket"
+  aws_region          = "us-east-1"
+  environment         = "dev"
+  bucket_name         = "xideraloscar-bucket-tf-practice"
+  web_index_file_name = "index.html"
+  web_error_file_name = "error.html"
+  bucket_policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "HTTP referer policy example",
+    Statement = [
+      {
+        Sid       = "AllowOnlySpecificReferers",
+        Effect    = "Allow",
+        Principal = "*",
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ],
+        Resource = "${aws_s3_bucket.main_bucket.arn}/*",
+    }]
+  })
+  web_html = {
+    path = ".\\web_files\\index.html",
+    name = "index.html",
+  }
+  web_css = {
+    path = ".\\web_files\\style.css"
+    name = "style.css"
+  }
+  web_js = {
+    path = ".\\web_files\\function.js"
+    name = "function.js"
+  }
+  block_public_access = false
 }
 
